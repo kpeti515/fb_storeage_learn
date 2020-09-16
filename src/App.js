c6967e67c369eeff285aaaaaa4c2a6399fe23559
Lorand <lorandkallai@gmail.com>
2020-09-16 20:02:55 +0200

    routes added, found weird bug Cannot set property 'href' of null if changing between pages multiple times too fast

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 import './App.css';
 import PswPage from './components/Psw_page'
-// import SupplierContactPage from './components/SupplierContact_page'
+import Home from './components/Home'
+import SupplierContactPage from './components/SupplierContact_page'
+import Route404 from './components/Route404'
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* <SupplierContactPage /> */}
-        <PswPage />
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/pswPage">PSW</Link>
+            </li>
+            <li>
+              <Link to="/supplierContacts">Névjegyzék</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/pswPage">
+          <PswPage />
+          </Route>
+          <Route path="/supplierContacts">
+          <SupplierContactPage />
+          </Route>
+          <Route path="/" exact="true">
+            <Home />
+          </Route>
+          <Route path="*">
+            <Route404 />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+        
+        
       </header>
     </div>
   );
---
+++ src/components/Route404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Route404 = () => {
+  return(
+    <React.Fragment>
+      <h2>Uuupsz</h2>
+      <p>A kért oldal nem található</p>
+      <Link to="/">Vissza a kezdőoldalra</Link>
+    </React.Fragment>
+  )
+}
+
+export default Route404
